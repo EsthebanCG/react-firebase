28e7b50b2ca92da0df51ca6a32855d5a8cf7f687
Estheban <developer02pachapp@gmail.com>
2020-08-21 22:57:16 -0500

    Updates

---
+++ precache-manifest.b9d576ece54e3184f254c179d36f91e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024f762ce4f73874dc2fea062bf8e8f4",
+    "url": "/react-firebase/index.html"
+  },
+  {
+    "revision": "615d692b8cc7bd51e25d",
+    "url": "/react-firebase/static/css/main.85a8c832.chunk.css"
+  },
+  {
+    "revision": "492ce77c835371db6e08",
+    "url": "/react-firebase/static/js/2.dbb51b16.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/react-firebase/static/js/2.dbb51b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "615d692b8cc7bd51e25d",
+    "url": "/react-firebase/static/js/main.afa9d01f.chunk.js"
+  },
+  {
+    "revision": "87e51ffc38d4e6979f5d",
+    "url": "/react-firebase/static/js/runtime-main.c30cc512.js"
+  }
+]);
